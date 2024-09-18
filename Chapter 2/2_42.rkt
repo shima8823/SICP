a9d2e85251a4ba116494163916caec6f9ce0872a
shima8823 <taiyaki1275@gmail.com>
2024-09-19 00:48:29 +0900

    Add safe? function

--- Chapter 2/2_42.rkt
+++ Chapter 2/2_42.rkt
@@ -2,6 +2,20 @@
 
 (define nil `())
 
+(define (abs x)
+	(cond ((< x 0) (- x))
+		(else x)))
+
+(define (list-ref items n)
+	(if (= n 0)
+		(car items)
+		(list-ref (cdr items) (- n 1))))
+
+(define (length items)
+	(if (null? items)
+		0
+		(+ 1 (length (cdr items)))))
+
 (define (enumerate-interval low high)
 	(if (> low high)
 		nil
@@ -26,6 +40,40 @@
 		(lambda (x) (not (= x item)))
 		sequence)
 )
+(define (safe? k positions)
+	(display "positions: ")(display positions)(newline)
+	(display "k: ")(display k)(newline)
+	; 列検知 同じ数字がないか
+	; 斜め検知 上左下右
+	; '((2 1) (1 2))
+	; (2 1)
+	(define (naname l i) ; i = 7, 6, 5... 2, 1
+		(let
+			(
+				(len (length l))
+			)
+			(cond
+				((<= i 0) #f)
+				(else
+					(or 
+						(= (abs (- (list-ref l 0) (list-ref l (- len i)))) (- len i))
+						(naname l (- i 1))
+					)
+				)
+			)
+		)
+	)
+
+	(cond
+		((= k 1) #t)
+		(else 
+			(and
+				(not (naname positions (- k 1)))
+				(safe? (- k 1) (cdr positions))
+			)
+		)
+	)
+)
 
 (define (queens board-size)
 	(define empty-board
@@ -34,22 +82,6 @@
 	(define (adjoin-position row col rest-of-queens)
 		; (append rest-of-queens col)
 		(cons col rest-of-queens)
-	)
-	(define (safe? k positions)
-		(display "position: ")(display positions)(newline)
-		; 列検知 同じ数字がないか
-		; 斜め検知 上左下右
-		; '((2 1) (1 2))
-		; (2 1)
-		(if (= k 1)
-			#t
-			(not (or
-				(= (car positions) (+ (cadr positions) 1))
-				(= (car positions) (- (cadr positions) 1))
-			))
-		)
-
-		
 	)
 	(define (queen-cols k)
 		(if (= k 0)
@@ -86,3 +118,6 @@
 |#
 
 (queens 2)
+
+(safe? 8 `(4 2 7 3 6 8 5 1))
+(safe? 8 `(4 2 7 3 6 8 5 7))
