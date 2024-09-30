153fd17e55b3aa95004fd01886c20dbdcfa39ebe
shima8823 <taiyaki1275@gmail.com>
2024-09-30 21:45:32 +0900

    Refactor complex number operations and fix missing procedure definitions

--- Chapter 2/generic-calc.rkt
+++ Chapter 2/generic-calc.rkt
@@ -1,23 +1,34 @@
 #lang racket
 
-(define *op-table* (make-hash))
+(define (square x) (* x x))
 
+(define *op-table* (make-hash))
 (define (put op type proc)
   (hash-set! *op-table* (list op type) proc))
-
 (define (get op type)
   (hash-ref *op-table* (list op type) #f))
 
-
-(define (attach-tag type-tag contents) (cons type-tag contents))
+(define (attach-tag type-tag contents) 
+	(if (eq? type-tag 'scheme-number)
+		contents
+		(cons type-tag contents)))
 (define (type-tag datum)
-	(if (pair? datum)
-		(car datum)
-		(error "Bad tagged datum: TYPE-TAG" datum)))
+	(cond ((pair? datum) (car datum))
+		  ((number? datum) 'scheme-number)
+		  (else (error "Bad tagged datum: TYPE-TAG" datum))))
 (define (contents datum)
-	(if (pair? datum)
-		(cdr datum)
-		(error "Bad tagged datum: CONTENTS" datum)))
+	(cond ((pair? datum) (cdr datum))
+		  ((number? datum) datum)
+		  (else (error "Bad tagged datum: CONTENTS" datum))))
+
+(define (apply-generic op . args)
+	(let ((type-tags (map type-tag args)))
+		(let ((proc (get op type-tags)))
+			(if proc
+				(apply proc (map contents args))
+				(error 
+					"No method for these types: APPLY-GENERIC"
+					(list op type-tags))))))
 
 (define (install-scheme-number-package)
 	(define (tag x) (attach-tag 'scheme-number x))
@@ -102,6 +113,56 @@
 	(put 'make-from-real-imag 'complex
 		(lambda (x y) (tag (make-from-real-imag x y))))
 	(put 'make-from-mag-ang 'complex
+		(lambda (r a) (tag (make-from-mag-ang r a))))
+
+	(put 'real-part '(complex) real-part)
+	(put 'imag-part '(complex) imag-part)
+	(put 'magnitude '(complex) magnitude)
+	(put 'angle '(complex) angle)
+	'done)
+
+(define (install-rectangular-package)
+	;; 内部手続き
+	(define (real-part z) (car z))
+	(define (imag-part z) (cdr z))
+	(define (make-from-real-imag x y) (cons x y))
+	(define (magnitude z)
+		(sqrt (+ (square (real-part z)) (square (imag-part z)))))
+	(define (angle z)
+		(atan (imag-part z) (real-part z)))
+	(define (make-from-mag-ang r a)
+		(cons (* r (cos a)) (* r (sin a))))
+	;; システムのほかの部分とのインターフェイス
+	(define (tag x) (attach-tag 'rectangular x))
+	(put 'real-part '(rectangular) real-part)
+	(put 'imag-part '(rectangular) imag-part)
+	(put 'magnitude '(rectangular) magnitude)
+	(put 'angle '(rectangular) angle)
+	(put 'make-from-real-imag 'rectangular
+		(lambda (x y) (tag (make-from-real-imag x y))))
+	(put 'make-from-mag-ang 'rectangular
+		(lambda (r a) (tag (make-from-mag-ang r a))))
+	'done)
+
+
+(define (install-polar-package)
+	;; 内部手続き
+	(define (magnitude z) (car z))
+	(define (angle z) (cdr z))
+	(define (make-from-mag-ang r a) (cons r a))
+	(define (real-part z) (* (magnitude z) (cos (angle z))))
+	(define (imag-part z) (* (magnitude z) (sin (angle z))))
+	(define (make-from-real-imag x y)
+		(cons (sqrt (+ (square x) (square y))) (atan y x)))
+	;; システムのほかの部分とのインターフェイス
+	(define (tag x) (attach-tag 'polar x))
+	(put 'real-part '(polar) real-part)
+	(put 'imag-part '(polar) imag-part)
+	(put 'magnitude '(polar) magnitude)
+	(put 'angle '(polar) angle)
+	(put 'make-from-real-imag 'polar
+		(lambda (x y) (tag (make-from-real-imag x y))))
+	(put 'make-from-mag-ang 'polar
 		(lambda (r a) (tag (make-from-mag-ang r a))))
 	'done)
 
