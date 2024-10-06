8573bb1ef6319f311ba675e88f9ccf6b140c13d0
shima8823 <taiyaki1275@gmail.com>
2024-10-06 17:52:07 +0900

    Add pesudoremainder-terms function. Instead of remainder-terms

--- Chapter 2/2_96.rkt
+++ Chapter 2/2_96.rkt
@@ -395,10 +395,19 @@
 			(error "Polys not in same var: DIV-POLY" (list p1 p2))))
 
 	(define (remainder-terms p1 p2) (cadr (div-terms p1 p2)))
+	(define (pseudoremainder-terms p q)
+		(cadr (div-terms
+			(let ((o1 (order (first-term p)))
+				  (o2 (order (first-term q)))
+				  (c  (coeff (first-term q))))
+				(mul-term-by-all-terms
+					(make-term 0 (expt c (+ 1 (- o1 o2))))
+					p))
+			q)))
 	(define (gcd-terms a b)
 		(if (empty-termlist? b)
 			a
-			(gcd-terms b (remainder-terms a b))))
+			(gcd-terms b (pseudoremainder-terms a b))))
 	(define (gcd-poly p1 p2)
 		(if (same-variable? (variable p1) (variable p2))
 			(make-poly (variable p1)
