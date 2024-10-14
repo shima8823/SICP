72735f95b7933c461665de7338473f9954fe83eb
shima8823 <taiyaki1275@gmail.com>
2024-10-14 22:25:08 +0900

    Add any demention lookup insert

--- Chapter 3/3_25.rkt
+++ Chapter 3/3_25.rkt
@@ -1,16 +1,18 @@
 #lang racket
 (require r5rs) ; set-(car|cdr)!
 
+#|
+
+(math -> ((* . 42) -> ((+ . 43) -> ((- . 45))))
+(math . complex)
+これは上書きされる
+
+コードを綺麗にするやる気がなくなった...
+
+|#
 
 (define (make-table)
-	(define (assoc key records)
-		(cond
-			((null? records) false)
-			((equal? key (caar records)) (car records))
-			(else (assoc key (cdr records)))))
-
 	(let ((local-table (list '*table*)))
-
 		(define (cons-keys keys value)
 			(cond
 				((= (length keys) 1) (cons (car keys) value))
@@ -18,42 +20,59 @@
 			))
 
 		;records = (cdr local-table) | subtable
-		(define (assoc-apply keys value records); f-record-found f-record-not-found f-subtable-not-found
-			(cond
-				; ((null? records) false)
-				((= (length keys) 1)
-					(let ((record (assoc (car keys) (cdr records))))
+		(define (assoc-apply keys value records prev)
+			(define (assoc key records)
+				(cond
+					((null? records) false)
+					((not (pair? (car records))) (assoc key (cdr records)))
+					((equal? key (caar records)) (car records))
+					(else (assoc key (cdr records)))))
+
+			(let ((record-or-subtable (assoc (car keys) records)))
+				(if (= (length keys) 1)
+					(let ((record record-or-subtable))
 						(if record
 							(set-cdr! record value)
-							(set-cdr! records (cons (cons (car keys) value) (cdr records))))))
-				(else
-					(let ((subtable
-						(assoc (car keys) records))) ;2回目だったら cdrしたい
-						(if subtable
-							(assoc-apply (cdr keys) value subtable); f-record-found f-record-not-found f-subtable-not-found
-							(set-cdr! local-table
-								; (cons (list (car keys) (cons (cdr keys) value)) ;(cons (cadr keys) (cons (caddr keys) value))
-								(cons (list (car keys) (cons-keys (cdr keys) value)) ;(cons (cadr keys) (cons (caddr keys) value))
-									(cdr local-table)))
+							(if (null? records)
+								(set-cdr! local-table (cons (list (car keys) value) (cdr local-table)))
+								(set-cdr! records (cons (cons (car keys) value) (if (null? records) records (cdr records)))))
 						)
 					)
-				)
-			)
-		)
+					(let ((subtable record-or-subtable))
+						(if subtable
+							(assoc-apply (cdr keys) value (cdr subtable) subtable)
+							(if (null? records)
+								(set-cdr! local-table
+									(cons (list (car keys) (cons-keys (cdr keys) value)) ;(cons (cadr keys) (cons (caddr keys) value))
+										(cdr local-table)))
+								(set-cdr! prev
+									(cons (list (car keys) (cons-keys (cdr keys) value)) records))
+							))))))
 
 		(define (lookup-assoc keys records)
+			(define (assoc key records)
+				(cond
+					((null? records) false)
+					((not (pair? (car records))) (assoc key (cdr records)))
+					((equal? key (caar records)) (car records))
+					(else (assoc key (cdr records)))))
+
 			(cond
 				((null? records) false)
 				((= (length keys) 1)
 					(let ((record (assoc (car keys) records)))
-						(if record
-							(cdr record)
+						(if record ; (complex . ()) or ('+' . 42)
+							(let ((cdr-record (cdr record)))
+								(if (and (pair? cdr-record) (= (length cdr-record) 1))
+									(car cdr-record)
+									cdr-record
+								))
 							false)))
 				(else
 					(let ((subtable
 						(assoc (car keys) records)))
 						(if subtable
-							(lookup-assoc (cdr keys) (cdr subtable)); f-record-found f-record-not-found f-subtable-not-found
+							(lookup-assoc (cdr keys) (cdr subtable))
 							false
 						)
 					)
@@ -61,32 +80,11 @@
 			)
 		)
 
-		; (define (lookup key-1 key-2)
 		(define (lookup keys)
-			; (let ((subtable
-			; 		(assoc key-1 (cdr local-table))))
-			; 	(if subtable
-			; 		(let ((record (assoc key-2 (cdr subtable))))
-			; 			(if record
-			; 				(cdr record)
-			; 				false))
-			; 		false))
-			(lookup-assoc keys (cdr local-table))
-		)
+			(lookup-assoc keys (cdr local-table)))
 
 		(define (insert! keys value)
-			; (let ((subtable
-			; 		(assoc key-1 (cdr local-table))))
-			; 	(if subtable
-			; 		(let ((record
-			; 			(assoc key-2 (cdr subtable))))
-			; 			(if record
-			; 				(set-cdr! record value)
-			; 				(set-cdr! subtable (cons (cons key-2 value) (cdr subtable)))))
-			; 		(set-cdr! local-table
-			; 			(cons (list key-1 (cons key-2 value))
-			; 				(cdr local-table)))))
-			(assoc-apply keys value (cdr local-table))
+			(assoc-apply keys value (cdr local-table) (cdr local-table))
 			'ok)
 
 		(define (dispatch m)
@@ -100,6 +98,7 @@
 (define put (operation-table 'insert-proc!))
 
 (put '(math) 'complex)
+(get '(math))
 
 (put '(math +) 43)
 (put '(math -) 45)
@@ -109,10 +108,12 @@
 (get '(math -))
 (get '(math *))
 
-(put '(country asia test) '(japanese chinese korean etc))
-; (put '(country asia japanese prefecture) '(Tokyo Kyoto Osaka etc))
-; (put '(country asia china prefecture) '(Beijin Shanghai etc))
-; (put '(country asia korea prefecture) '(Busan Pyonyan etc))
+(put '(country asia) '(japanese chinese korean etc))
+(put '(country asia japanese prefecture) '(Tokyo Kyoto Osaka etc))
+(put '(country asia china prefecture) '(Beijin Shanghai etc))
+(put '(country asia korea prefecture) '(Busan Pyonyan etc))
 
-(get '(country asia))
-
+(display (get '(country asia)))(newline)
+(display (get '(country asia japanese prefecture)))(newline)
+(display (get '(country asia china prefecture)))(newline)
+(display (get '(country asia korea prefecture)))(newline)
