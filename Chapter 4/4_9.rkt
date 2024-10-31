2c35b450e04cec813ad1f7f72aba5990b49ea37d
shima8823 <taiyaki1275@gmail.com>
2024-11-01 00:29:21 +0900

    Add test

--- Chapter 4/4_9.rkt
+++ Chapter 4/4_9.rkt
@@ -55,7 +55,7 @@ sequence->exp
 (define (do-variable-set v) (caddr v))
 (define (do-body exp) (cdddr exp))
 (define (do-condtion exp) (car (caddr exp)))
-(define (do-return-value exp) (cadr (caddr exp)))
+(define (do-return-value exp) (cdr (caddr exp)))
 
 (define (do-make-body body variables)
 	(list
@@ -68,14 +68,14 @@ sequence->exp
 						(list 'set! (do-variable-name v) (do-variable-set v)))
 					variables)))))
 
-(define (do-make-loop body condtion return-value)
+(define (do-make-loop condtion return-value)
 	(list
 		'define '(loop) 
 		(list
 			'if
 			; (list 'true? condtion) 
 			condtion
-			return-value
+			(sequence->exp return-value)
 			(sequence->exp (list '(body) '(loop)))
 			)
 		))
@@ -85,36 +85,43 @@ sequence->exp
 		'let
 		(map (lambda (v) (list (do-variable-name v) (do-variable-init v))) (do-variables exp))
 		(do-make-body (do-body exp) (do-variables exp))
-		(do-make-loop (do-body exp) (do-condtion exp) (do-return-value exp))
+		(do-make-loop (do-condtion exp) (do-return-value exp))
 		(sequence->exp (list (sequence->exp (do-body exp)) '(loop)))
 		))
 
-(do->let
-		'(do ((i 1 (+ i 1))
+(define test (do->let '
+	(do ((i 1 (+ i 1))
 			(sum 0 (+ sum i)))
 			((> i 10) sum)
 			(display i)(newline)
-			))
+			)
+))
+
+; test1
+; 		(do ((i 1 (+ i 1))
+; 			(sum 0 (+ sum i)))
+; 			((> i 10) sum)
+; 			(display i)(newline)
+; 			))
+
+; TODO: test2 (j 2)
+; (do ((i 0 (+ i 1)) (j 2))
+;     ((> i 10) (newline) (display i) j)
+;     (newline) (display i) (set! i (+ i 2)))
+
+test
 
 (define ns (make-base-namespace))
 
 (eval '(define (last-exp? seq) (null? (cdr seq))) ns)
 (eval '(define (make-begin seq) (cons 'begin seq)) ns)
 (eval '(define (sequence->exp seq)
-		(display seq)(newline)
          (cond ((null? seq) seq)
                ((last-exp? seq) (first-exp seq))
                (else (make-begin seq))))
       ns)
 
-(eval
-	(do->let
-		'(do ((i 1 (+ i 1))
-			(sum 0 (+ sum i)))
-			((> i 10) sum)
-			(display i)(newline)
-			))
-	ns)
+(eval test ns)
 
 #|
 
