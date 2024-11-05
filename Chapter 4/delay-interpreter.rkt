974c17f80575ba01ddbcf967b11078867120ea2a
shima8823 <taiyaki1275@gmail.com>
2024-11-05 21:31:49 +0900

    Add L-Eval

--- Chapter 4/delay-interpreter.rkt
+++ Chapter 4/delay-interpreter.rkt
@@ -22,21 +22,55 @@
 		((cond? exp) (eval (cond->if exp) env))
 		((application? exp)
 			(self-apply
-				(eval (operator exp) env)
-				(list-of-values (operands exp) env)))
+					(actual-value (operator exp) env)
+					(operands exp)
+					env))
 		(else
 			(error " Unknown expression type : EVAL " exp))))
 
-(define (self-apply procedure arguments)
+(define (actual-value exp env)
+	(force-it (eval exp env)))
+
+(define (force-it obj)
+	(if (thunk? obj)
+		(actual-value (thunk-exp obj) (thunk-env obj))
+		obj))
+
+(define (delay-it exp env)
+	(list 'thunk exp env))
+(define (thunk? obj)
+	(tagged-list? obj 'thunk))
+(define (thunk-exp thunk) (cadr thunk))
+(define (thunk-env thunk) (caddr thunk))
+
+(define (list-of-arg-values exps env)
+	(if (no-operands? exps)
+		'()
+		(cons (actual-value (first-operand exps)
+				env)
+			  (list-of-arg-values (rest-operands exps)
+				env))))
+
+(define (list-of-delayed-args exps env)
+	(if (no-operands? exps)
+		'()
+		(cons (delay-it (first-operand exps)
+				env)
+			  (list-of-delayed-args (rest-operands exps)
+				env))))
+
+(define (self-apply procedure arguments env)
 	(cond
 		((primitive-procedure? procedure)
-			(apply-primitive-procedure procedure arguments))
+			(apply-primitive-procedure
+				procedure
+				(list-of-arg-values arguments env))) ; changed))
 		((compound-procedure? procedure)
 			(eval-sequence
 				(procedure-body procedure)
 				(extend-environment
 					(procedure-parameters procedure)
-					arguments
+					(list-of-delayed-args arguments env) ; changed
 					(procedure-environment procedure))))
 		(else
 			(error "Unknown procedure type : APPLY " procedure))))
@@ -48,7 +82,7 @@
 			  (list-of-values (rest-operands exps) env))))
 
 (define (eval-if exp env)
-	(if (true? (eval (if-predicate exp) env))
+	(if (true? (actual-value (if-predicate exp) env))
 		(eval (if-consequent exp) env)
 		(eval (if-alternative exp) env)))
 
@@ -281,12 +315,12 @@
 	(apply-in-underlying-scheme
 		(primitive-implementation proc) args))
 
-(define input-prompt "~~~ M-Eval input :")
-(define output-prompt "~~~ M-Eval value :")
+(define input-prompt "~~~ L-Eval input :")
+(define output-prompt "~~~ L-Eval value :")
 (define (driver-loop)
 	(prompt-for-input input-prompt)
 	(let ((input (read)))
-		(let ((output (eval input the-global-environment)))
+		(let ((output (actual-value input the-global-environment)))
 			(announce-output output-prompt)
 			(user-print output)))
 	(driver-loop))
