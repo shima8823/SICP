5969e4c264dc8bcac7a0fe225fab196010f6dc36
shima8823 <taiyaki1275@gmail.com>
2024-11-10 21:55:13 +0900

    Add complex parser

--- Chapter 4/amb-parse.rkt
+++ Chapter 4/amb-parse.rkt
@@ -1,3 +1,4 @@
+#lang sicp
 
 (define (memq item x)
 	(cond ((null? x) false)
@@ -9,16 +10,38 @@
 (define nouns '(noun student professor cat class))
 (define verbs '(verb studies lectures eats sleeps))
 (define articles '(article the a))
+(define prepositions '(prep for to in by with))
 
 (define (parse-sentence)
-	(list 'sentence
-		(parse-noun-phrase)
-		(parse-word verbs)))
+	(list 'sentence (parse-noun-phrase) (parse-verb-phrase)))
 
-(define (parse-noun-phrase)
-	(list 'noun-phrase
+(define (parse-simple-noun-phrase)
+	(list 'simple-noun-phrase
 		(parse-word articles)
 		(parse-word nouns)))
+
+(define (parse-noun-phrase)
+	(define (maybe-extend noun-phrase)
+		(amb noun-phrase
+			(maybe-extend
+				(list 'noun-phrase
+					noun-phrase
+					(parse-prepositional-phrase)))))
+	(maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-verb-phrase)
+	(define (maybe-extend verb-phrase)
+		(amb verb-phrase
+			(maybe-extend
+				(list 'verb-phrase
+					verb-phrase
+					(parse-prepositional-phrase)))))
+	(maybe-extend (parse-word verbs)))
+
+(define (parse-prepositional-phrase)
+	(list 'prep-phrase
+		(parse-word prepositions)
+		(parse-noun-phrase)))
 
 (define (parse-word word-list)
 	(require (not (null? *unparsed*)))
@@ -34,3 +57,22 @@
 		(require (null? *unparsed*)) sent))
 
 (parse '(the cat eats))
+(parse '(the student with the cat sleeps in the class))
+
+#|
+
+(sentence
+  (noun-phrase
+    (simple-noun-phrase (article the) (noun student))
+	(prep-phrase
+	  (prep with)
+	  (simple-noun-phrase (article the) (noun cat))))
+	(verb-phrase
+	  (verb sleeps)
+	  (prep-phrase
+	  	(prep in)
+		(simple-noun-phrase (article the) (noun class)))))
+
+|#
+
+(parse '(the professor lectures to the student with the cat))
