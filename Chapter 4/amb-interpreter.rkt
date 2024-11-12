7c3f09a8003e730849e16b41195bc4d6937638a9
shima8823 <taiyaki1275@gmail.com>
2024-11-12 15:32:29 +0900

    feat analyze-amb

--- Chapter 4/amb-interpreter.rkt
+++ Chapter 4/amb-interpreter.rkt
@@ -471,4 +471,16 @@
 (define (amb? exp) (tagged-list? exp 'amb))
 (define (amb-choices exp) (cdr exp))
 
+(define (analyze-amb exp)
+	(let ((cprocs (map analyze (amb-choices exp))))
+		(lambda (env succeed fail)
+			(define (try-next choices)
+				(if (null? choices)
+					(fail)
+					((car choices)
+						env
+						succeed
+						(lambda () (try-next (cdr choices))))))
+			(try-next cprocs))))
+
 (driver-loop)
