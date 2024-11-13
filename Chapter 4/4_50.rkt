38b580130ea07495697a5a8d0b5858c032a75a22
shima8823 <taiyaki1275@gmail.com>
2024-11-13 16:56:30 +0900

    feat ramb instead of amb

--- Chapter 4/4_50.rkt
+++ Chapter 4/4_50.rkt
@@ -17,6 +17,7 @@
 		((cond? exp) (analyze (cond->if exp)))
 		((let? exp) (analyze (let->combination exp)))
 		((amb? exp) (analyze-amb exp))
+		((ramb? exp) (analyze-ramb exp))
 		((application? exp) (analyze-application exp))
 		(else (error " Unknown expression type : ANALYZE " exp))))
 
@@ -502,4 +503,105 @@
 						(lambda () (try-next (cdr choices))))))
 			(try-next cprocs))))
 
+(define (ramb? exp) (tagged-list? exp 'ramb))
+(define (ramb-choices exp) (cdr exp))
+
+(define (remove-index index sequence)
+	(let ((len (length sequence)))
+		(define (iter prev seq)
+			(cond ((null? seq) '())
+				  ((= (- len index) (length seq))
+				  	(if (null? prev)
+						(cdr seq)
+						(append prev (cdr seq))))
+				  (else (iter (append prev (list (car seq))) (cdr seq)))))
+		(iter '() sequence)))
+
+; simple 別解 https://sicp.iijlab.net/solution/ex4.3.html#ex4.50
+(define (list-rem list n)
+    (if (= n 0) (cdr list)
+        (cons (car list) (list-rem (cdr list) (- n 1)))))
+
+(define (analyze-ramb exp)
+	(let ((cprocs (map analyze (amb-choices exp))))
+		(lambda (env succeed fail)
+			(define (try-next choices)
+				(if (null? choices)
+					(fail)
+					(let ((index (random (length choices))))
+						((list-ref choices index)
+							env
+							succeed
+							(lambda () (try-next (remove-index index choices)))))))
+			(try-next cprocs))))
+
 (driver-loop)
+
+#|
+
+input from 4_49.rkt
+
+(define (memq item x)
+	(cond ((null? x) false)
+		  ((eq? item (car x)) x)
+		  (else (memq item (cdr x)))))
+
+(define (require p) (if (not p) (ramb)))
+
+(define nouns '(noun student professor cat class))
+(define verbs '(verb studies lectures eats sleeps))
+(define articles '(article the a))
+(define prepositions '(prep for to in by with))
+
+(define (parse-sentence)
+	(list 'sentence (parse-noun-phrase) (parse-verb-phrase)))
+
+(define (parse-simple-noun-phrase)
+	(list 'simple-noun-phrase
+		(parse-word articles)
+		(parse-word nouns)))
+
+(define (parse-noun-phrase)
+	(define (maybe-extend noun-phrase)
+		(ramb noun-phrase
+			(maybe-extend
+				(list 'noun-phrase
+					noun-phrase
+					(parse-prepositional-phrase)))))
+	(maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-verb-phrase)
+	(define (maybe-extend verb-phrase)
+		(ramb verb-phrase
+			(maybe-extend
+				(list 'verb-phrase
+					verb-phrase
+					(parse-prepositional-phrase)))))
+	(maybe-extend (parse-word verbs)))
+
+(define (parse-prepositional-phrase)
+	(list 'prep-phrase
+		(parse-word prepositions)
+		(parse-noun-phrase)))
+
+(define (parse-word word-list)
+	(define (expansion-words words)
+		(require (not (null? words)))
+		(ramb (car words)
+			(expansion-words (cdr words))))
+
+	(require (not (null? *unparsed*)))
+	(set! *unparsed* (cdr *unparsed*))
+	(list (car word-list) (expansion-words (cdr word-list))))
+
+(define *unparsed* '())
+(define (parse input)
+	(set! *unparsed* input)
+	(let ((sent (parse-sentence)))
+		(require (null? *unparsed*)) sent))
+
+(parse '(the cat eats))
+
+try-again
+
+|#
