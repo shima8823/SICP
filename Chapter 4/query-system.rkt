faf4bdb1a525f5ac365bda108850891c0fd3cd1e
shima8823 <taiyaki1275@gmail.com>
2024-11-19 13:06:58 +0900

    Add 4.4.4.3 ~ 4

--- Chapter 4/query-system.rkt
+++ Chapter 4/query-system.rkt
@@ -38,6 +38,8 @@
 			(else exp)))
 	(copy exp))
 
+; 4.4.4.2
+
 (define (qeval query frame-stream)
 	(let ((qproc (get (type query) 'qeval)))
 		(if qproc
@@ -98,3 +100,96 @@
 
 (define (always-true ignore frame-stream) frame-stream)
 (put 'always-true 'qeval always-true)
+
+; 4.4.4.3
+
+(define (find-assertions pattern frame)
+	(stream-flatmap
+		(lambda (datum) (check-an-assertion datum pattern frame))
+		(fetch-assertions pattern frame)))
+
+(define (check-an-assertion assertion query-pat query-frame)
+	(let ((match-result (pattern-match query-pat assertion query-frame)))
+		(if (eq? match-result 'failed)
+			the-empty-stream
+			(singleton-stream match-result))))
+
+(define (pattern-match pat dat frame)
+	(cond ((eq? frame 'failed) 'failed)
+		  ((equal? pat dat) frame)
+		  ((var? pat) (extend-if-consistent pat dat frame))
+		  ((and (pair? pat) (pair? dat))
+			(pattern-match
+				(cdr pat)
+				(cdr dat)
+				(pattern-match (car pat) (car dat) frame)))
+		  (else 'failed)))
+
+(define (extend-if-consistent var dat frame)
+	(let ((binding (binding-in-frame var frame)))
+		(if binding
+			(pattern-match (binding-value binding) dat frame)
+			(extend var dat frame))))
+
+; 4.4.4.4
+
+(define (apply-rules pattern frame)
+	(stream-flatmap
+		(lambda (rule) (apply-a-rule rule pattern frame))
+		(fetch-rules pattern frame)))
+
+(define (apply-a-rule rule query-pattern query-frame)
+	(let ((clean-rule (rename-variables-in rule)))
+		(let ((unify-result (unify-match query-pattern (conclusion clean-rule) query-frame)))
+			(if (eq? unify-result 'failed)
+				the-empty-stream
+				(qeval
+					(rule-body clean-rule)
+					(singleton-stream unify-result))))))
+
+(define (rename-variables-in rule)
+	(let ((rule-application-id (new-rule-application-id)))
+		(define (tree-walk exp)
+			(cond ((var? exp) (make-new-variable exp rule-application-id))
+				  ((pair? exp) (cons (tree-walk (car exp)) (tree-walk (cdr exp))))
+				  (else exp)))
+		(tree-walk rule)))
+
+(define (unify-match p1 p2 frame)
+	(cond ((eq? frame 'failed) 'failed)
+		  ((equal? p1 p2) frame)
+		  ((var? p1) (extend-if-possible p1 p2 frame))
+		  ((var? p2) (extend-if-possible p2 p1 frame)) ; ***
+		  ((and (pair? p1) (pair? p2))
+			(unify-match
+				(cdr p1)
+				(cdr p2)
+				(unify-match (car p1) (car p2) frame)))
+		  (else 'failed)))
+
+(define (extend-if-possible var val frame)
+	(let ((binding (binding-in-frame var frame)))
+		(cond (binding (unify-match (binding-value binding) val frame))
+			  ((var? val) ; ***
+				(let ((binding (binding-in-frame val frame)))
+					(if binding
+						(unify-match var (binding-value binding) frame)
+						(extend var val frame))))
+			  ((depends-on? val var frame) ; ***
+				'failed)
+			  (else (extend var val frame)))))
+
+(define (depends-on? exp var frame)
+	(define (tree-walk e)
+		(cond 
+			((var? e)
+				(if (equal? var e)
+					true
+					(let ((b (binding-in-frame e frame)))
+						(if b
+							(tree-walk (binding-value b))
+							false))))
+			((pair? e)
+				(or (tree-walk (car e)) (tree-walk (cdr e))))
+			(else false)))
+	(tree-walk exp))
