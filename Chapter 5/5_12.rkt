30f86eb40f98ce78a2593638fa93e19cacebcb89
shima8823 <taiyaki1275@gmail.com>
2024-12-10 14:45:38 +0900

    add debug fact-machine

--- Chapter 5/5_12.rkt
+++ Chapter 5/5_12.rkt
@@ -433,25 +433,35 @@
 
 
 ; 階乗マシン
-; (controller
-; (assign continue (label fact-done)) ;set up final return address
-; fact-loop
-; (test (op =) (reg n) (const 1))
-; (branch (label base-case))
-; ;; Set up for the recursive call by saving n and continue.
-; ;; Set up continue so that the computation will continue
-; ;; at after-fact when the subroutine returns.
-; (save continue)
-; (save n)
-; (assign n (op -) (reg n) (const 1))
-; (assign continue (label after-fact))
-; (goto (label fact-loop))
-; after-fact
-; (restore n)
-; (restore continue)
-; (assign val (op *) (reg n) (reg val)) ;val now contains n(n - 1)!
-; (goto (reg continue)) ;return to caller
-; base-case
-; (assign val (const 1)) (goto (reg continue)) ;base case: 1! = 1
-; ;return to caller
-; fact-done)
+(define fact-machine
+	(make-machine
+		'(n val continue)
+		(list
+			(list '= =)
+			(list '- -)
+			(list '* *))
+		'((assign continue (label fact-done))
+		  fact-loop
+			(test (op =) (reg n) (const 1))
+			(branch (label base-case))
+			;; Set up for the recursive call by saving n and continue.
+			;; Set up continue so that the computation will continue
+			;; at after-fact when the subroutine returns.
+			(save continue)
+			(save n)
+			(assign n (op -) (reg n) (const 1))
+			(assign continue (label after-fact))
+			(goto (label fact-loop))
+		  after-fact
+			(restore n)
+			(restore continue)
+			(assign val (op *) (reg n) (reg val)) ;val now contains n(n - 1)!
+			(goto (reg continue)) ;return to caller
+		  base-case
+			(assign val (const 1)) (goto (reg continue)) ;base case: 1! = 1
+		  fact-done)))
+
+(get-insts fact-machine)
+(get-entrypoints fact-machine)
+(get-regs-stacked fact-machine)
+(get-regs-assigned fact-machine)
