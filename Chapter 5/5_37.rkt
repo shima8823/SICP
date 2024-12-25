824b086cedc3ab6f88c9675347ef3e5f7dee53a3
shima8823 <taiyaki1275@gmail.com>
2024-12-25 17:27:40 +0900

    add solve

--- Chapter 5/5_37.rkt
+++ Chapter 5/5_37.rkt
@@ -308,19 +308,18 @@
 	(if (null? regs)
 		(append-instruction-sequences seq1 seq2)
 		(let ((first-reg (car regs)))
-			(if (and (needs-register? seq2 first-reg)
-					 (modifies-register? seq1 first-reg))
-				(preserving (cdr regs)
-					(make-instruction-sequence
-						(list-union (list first-reg)
-									(registers-needed seq1))
-						(list-difference (registers-modified seq1)
-										 (list first-reg))
-						(append `((save ,first-reg))
-								(statements seq1)
-								`((restore ,first-reg))))
-					seq2)
-				(preserving (cdr regs) seq1 seq2)))))
+			; (if (and (needs-register? seq2 first-reg)
+			; 		 (modifies-register? seq1 first-reg))
+			(preserving (cdr regs)
+				(make-instruction-sequence
+					(list-union (list first-reg)
+								(registers-needed seq1))
+					(list-difference (registers-modified seq1)
+									(list first-reg))
+					(append `((save ,first-reg))
+							(statements seq1)
+							`((restore ,first-reg))))
+				seq2))))
 
 (define (tack-on-instruction-sequence seq body-seq)
 	(make-instruction-sequence
@@ -340,9 +339,91 @@
 
 (display-insts
 (compile
-	'(define (factorial n)
-		(if (= n 1)
-			1
-			(* (factorial (- n 1)) n)))
+	'(define (f x)
+			(+ x 2))
 	'val
 	'next))
+
+#|
+
+特定についてはdiffで。
+明らかにstack操作が増えていて、大部分はいらない。
+
+////// original preserve
+
+
+(assign val (op make-compiled-procedure) (label entry1) (reg env))
+(goto (label after-lambda2))
+entry1
+(assign env (op compiled-procedure-env) (reg proc))
+(assign env (op extend-environment) (const (x)) (reg argl) (reg env))
+(assign proc (op lookup-variable-value) (const +) (reg env))
+(assign val (const 2))
+(assign argl (op list) (reg val))
+(assign val (op lookup-variable-value) (const x) (reg env))
+(assign argl (op cons) (reg val) (reg argl))
+(test (op primitive-procedure?) (reg proc))
+(branch (label primitive-branch3))
+compiled-branch4
+(assign val (op compiled-procedure-entry) (reg proc))
+(goto (reg val))
+primitive-branch3
+(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+(goto (reg continue))
+after-call5
+after-lambda2
+(perform (op define-variable!) (const f) (reg val) (reg env))
+(assign val (const ok))
+
+////// 非効率preserve
+
+
+(save continue)
+(save env)
+(save continue)
+(assign val (op make-compiled-procedure) (label entry1) (reg env))
+(restore continue)
+(goto (label after-lambda2))
+entry1
+(assign env (op compiled-procedure-env) (reg proc))
+(assign env (op extend-environment) (const (x)) (reg argl) (reg env))
+(save continue)
+(save env)
+(save continue)
+(assign proc (op lookup-variable-value) (const +) (reg env))
+(restore continue)
+(restore env)
+(restore continue)
+(save continue)
+(save proc)
+(save env)
+(save continue)
+(assign val (const 2))
+(restore continue)
+(assign argl (op list) (reg val))
+(restore env)
+(save argl)
+(save continue)
+(assign val (op lookup-variable-value) (const x) (reg env))
+(restore continue)
+(restore argl)
+(assign argl (op cons) (reg val) (reg argl))
+(restore proc)
+(restore continue)
+(test (op primitive-procedure?) (reg proc))
+(branch (label primitive-branch3))
+compiled-branch4
+(assign val (op compiled-procedure-entry) (reg proc))
+(goto (reg val))
+primitive-branch3
+(save continue)
+(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+(restore continue)
+(goto (reg continue))
+after-call5
+after-lambda2
+(restore env)
+(perform (op define-variable!) (const f) (reg val) (reg env))
+(assign val (const ok))
+(restore continue)
+|#
